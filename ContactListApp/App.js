35842a33a559cdf2d0306eeda9e5e8af47ed8eff
Leeloo Schaff <leeloo.schaff@gmail.com>
2024-11-24 16:38:03 +1100

    Files creation

--- ContactListApp/App.js
+++ ContactListApp/App.js
@@ -1,20 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStaticNavigator } from '@react-navigation/native';
+import ContactListScreen from './screens/ContactListScreen';
+import ContactDetailsScreen from './screens/ContactDetailsScreen'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ContactList" component="ContactListScreen" options={{title:'Contacts'}} />
+        <Stack.Screen name="ContactDetails" component="ContactDetailsScreen" options={{title:'Contact Details'}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
