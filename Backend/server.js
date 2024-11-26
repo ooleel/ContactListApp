e66c06f8d1589185ab724368a7c949b78ea02ce6
Leeloo Schaff <leeloo.schaff@gmail.com>
2024-11-26 13:41:15 +1100

    feedback 1 and backend

---
+++ Backend/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors'); //communication between React Native and backend
+const bodyParser = require('body-parser'); //parses incoming request bodies in JSON format
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//data 
+let contacts = [
+        {
+            id: '1', 
+            name: 'John Smith', 
+            phone: '02 9988 2211',
+            department: 'Information Communications Technology',
+            address: {
+                street: '1 Code Lane',
+                city: 'Javaville',
+                state: 'NSW',
+                zip: '0100',
+                country: 'Australia'
+            }
+        },
+        {
+            id: '2', 
+            name: 'Sue White', 
+            phone: '03 8899 2255',
+            department: 'Finance',
+            address: {
+                street: '16 Bit Way',
+                city: 'Byte Cove',
+                state: 'QLD',
+                zip: '1101',
+                country: 'Australia'
+            },
+        },
+    ];
+
+//routes
+app.get('/contacts', (req, res) => {
+    res.json(contacts);
+});
+
+app.post('/contacts', (req, res) => {
+    const newContact = {id: contacts.length + 1, ...req.body};
+    contacts.push(newContact);
+    res.status(201).json(newContact);
+});
+
+app.put('/contacts/:id', (req, res) => { //only post!!
+    const {id} = req.params;
+    const {name, phone} = req.body;
+    const contact = contacts.find//HERE!!!!!!!!
+})
+
+app.listen(3000, function() {
+    console.log("Server is now running");
+
+});
